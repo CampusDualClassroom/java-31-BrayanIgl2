33ba92b219b7270498b633a9a4b8361e48dad8ee
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-27 11:32:13 +0200

    31 ok

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,31 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
 
+        try {
+
+            //Se abre un bufferedReader para leer el archivo
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"));
+
+            String line;
+
+            //Se recorren las lineas del archivo
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
